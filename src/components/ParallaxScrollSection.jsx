373d8a0fccf84eecc668471787d02b56dcbd61b9
AbelMP17 <abelmp890@gmail.com>
2025-06-04 22:54:57 +0200

    mobile bubble fixes

--- src/components/ParallaxScrollSection.jsx
+++ src/components/ParallaxScrollSection.jsx
@@ -186,7 +186,7 @@ export default function ParallaxScrollSection() {
   return (
     <div className="flex flex-col md:flex-row">
       {/* IZQUIERDA */}
-      <div ref={wrapperRef} className="relative w-full md:w-1/2 h-[300vh]">
+      <div ref={wrapperRef} className="relative w-full md:w-1/2 h-[100vh] md:h-[300vh]">
         <div className="sticky top-0 h-screen flex justify-center items-center w-full">
           <div className="relative w-full flex justify-center items-center h-full pointer-events-none md:pointer-events-auto overflow-hidden">
             <div
